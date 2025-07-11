470a11459f4e0b2307f9273884f484c11194923a
Rémi Dupuy <rdu@rdu-mbp.local>
2025-07-11 12:18:15 +0200

    redux store setup

---
+++ nextjs/src/redux/store.ts
@@ -0,0 +1,35 @@
+import { configureStore, ThunkAction, Action, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: [],
+};
+
+const rootReducer = combineReducers({
+    nullReducer: (state = null) => state, // Placeholder for errors while no reducers are defined
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
